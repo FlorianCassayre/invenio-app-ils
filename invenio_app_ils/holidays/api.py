8a52ef2d98d2594ef91bea4049344281a4907e73
Florian Cassayre <florian.cassayre@gmail.com>
2020-09-14 16:49:35 +0200

    Validation and API definition

---
+++ invenio_app_ils/holidays/api.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+#
+# invenio-app-ils is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""ILS holidays API."""
+
+
+def is_open(location_pid, date):
+    """Checks if the location is open or closed on a given date."""
+    pass
+
+
+def next_open(location_pid, start_date):
+    """Finds the next open day where this location is open."""
+    pass
--- invenio_app_ils/locations/loaders/jsonschemas/location.py
+++ invenio_app_ils/locations/loaders/jsonschemas/location.py
@@ -9,7 +9,7 @@
 
 from invenio_records_rest.schemas import RecordMetadataSchemaJSONV1
 from invenio_records_rest.schemas.fields import PersistentIdentifier
-from marshmallow import EXCLUDE, Schema, fields
+from marshmallow import EXCLUDE, Schema, fields, pre_load, ValidationError, post_load
 
 
 class OpeningWeekdaySchema(Schema):
@@ -37,6 +37,13 @@ class OpeningExceptionSchema(Schema):
     start_date = fields.Date(required=True)
     end_date = fields.Date(required=True)
 
+    @pre_load
+    def validate_dates(self, data, **kwargs):
+        """Validate dates."""
+        if data["end_date"] < data["start_date"]:
+            raise ValidationError("End date cannot happen before start date.",
+                                  field_names=["start_date", "end_date"])
+
 
 class LocationSchemaV1(RecordMetadataSchemaJSONV1):
     """Location schema."""
@@ -49,3 +56,16 @@ class LocationSchemaV1(RecordMetadataSchemaJSONV1):
     notes = fields.Str()
     opening_weekdays = fields.List(fields.Nested(OpeningWeekdaySchema))
     opening_exceptions = fields.List(fields.Nested(OpeningExceptionSchema))
+
+    @post_load
+    def postload_checks(self, data, **kwargs):
+        """Validate record."""
+        record = self.context["record"]
+        exceptions = record["opening_exceptions"]
+        for i in range(len(exceptions)):
+            exception = exceptions[i]
+            if i > 0:
+                previous = exceptions[i - 1]
+                if previous["end_date"] >= exception["start_date"]:
+                    raise ValidationError("Exceptions must be sorted and not overlap.",
+                                          field_names=["opening_exceptions"])
